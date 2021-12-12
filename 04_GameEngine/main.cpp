89c7c200da5d9388018fdddc6ba18570f333dee8
Anton Meineche Falk <anton.m.falk.illustration@gmail.com>
2021-12-12 21:46:54 +0100

    poggers game engine frame limiting

---
+++ 04_GameEngine/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <windows.h>
+
+
+class Game
+{
+public:
+    void Run()
+    {
+        std::cout << "Game::Tick" << std::endl;
+
+        BeginPlay();
+
+        while (true)
+        {
+            Tick();
+
+            Sleep(10);
+        }
+    }
+
+
+    void BeginPlay()
+    {
+        std::cout << "Game::BeginPlay()" << std::endl;
+
+    }
+
+
+    void Tick()
+    {
+        std::cout << "Game::Tick" << std::endl;
+
+    }
+
+};
+
+
+int main(int argc, char* argv[])
+{
+    Game game;
+    game.Run();
+
+    return 0;
+}
